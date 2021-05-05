00c2bb0c7028b162e26b02b509abe608b30c7872
Gonzalo <100429091@alumnos.uc3m.es>
2021-05-05 14:55:43 +0200

    Open_door refactorizado

--- src/main/python/secure_all/access_manager.py
+++ src/main/python/secure_all/access_manager.py
@@ -7,9 +7,8 @@ from secure_all.exception.access_management_exception import AccessManagementExc
 from secure_all.data.access_key import AccessKey
 from secure_all.data.access_request import AccessRequest
 from secure_all.cfg.access_manager_config import JSON_FILES_PATH
-from .data.attribute_dni import Dni
-from .data.attribute_access_code import AccessCode
-from .data.attribute_key_labels import KeyLabels
+from secure_all.data.attribute_key import Key
+from secure_all.storage.keys_json_store import KeysJsonStore
 
 
 class AccessManager:
@@ -32,22 +31,6 @@ class AccessManager:
         return data
 
     @staticmethod
-    def find_credentials(dni):
-        """ return the access request related to a given dni"""
-        requests_store = JSON_FILES_PATH + "storeRequest.json"
-        try:
-            with open(requests_store, "r", encoding="utf-8", newline="") as file:
-                list_data = json.load(file)
-        except FileNotFoundError as ex:
-            raise AccessManagementException("Wrong file or file path") from ex
-        except json.JSONDecodeError as ex:
-            raise AccessManagementException("JSON Decode Error - Wrong JSON Format") from ex
-        for request in list_data: 
-            if request["_AccessRequest__id_document"] == dni:
-                return request
-        return None
-
-    @staticmethod
     def request_access_code(id_card, name_surname, access_type, email_address, days):
         """ this method give access to the building"""
 
@@ -55,42 +38,15 @@ class AccessManager:
         my_request.store_request()
         return my_request.access_code
 
-    def get_access_key(self, key_file):
+    @staticmethod
+    def get_access_key(key_file):
         my_key = AccessKey(key_file)
         # store the key generated.
         my_key.store_keys()
         return my_key.key
 
-    def validate_access_code_for_dni(self, request_code, dni):
-        # Comrpobar que el dni es correcto
-        Dni(dni)
-        # check if this dni is stored, and return in credentials all the info
-        credentials = self.find_credentials(dni)
-        if credentials is None:
-            raise AccessManagementException("DNI is not found in the store")
-        # generate the access code to check if it is correct
-        access_request = AccessRequest(credentials['_AccessRequest__id_document'],
-                                       credentials['_AccessRequest__name'],
-                                       credentials['_AccessRequest__visitor_type'],
-                                       credentials['_AccessRequest__email_address'],
-                                       credentials['_AccessRequest__validity'])
-        access_code = access_request.access_code
-        if access_code != request_code:
-            raise AccessManagementException("access code is not correct for this DNI")
-        return credentials
-
-    def open_door(self, key):
-        # check if key is complain with the  correct format
-        regex = r'[0-9a-f]{64}'
-        if not re.fullmatch(regex, key):
-            raise AccessManagementException("key invalid")
-        keys_store = JSON_FILES_PATH + "storeKeys.json"
-        keys_from_store = self.read_key_file(keys_store)
-        justnow = datetime.utcnow()
-        justnow_timestamp = datetime.timestamp(justnow)
-        for key_in_store in keys_from_store:
-            if key_in_store["_AccessKey__key"] == key \
-                    and (key_in_store["_AccessKey__expiration_date"] > justnow_timestamp
-                         or key_in_store["_AccessKey__expiration_date"] == 0):
-                return True
-        raise AccessManagementException("key is not found or is expired")
+    @staticmethod
+    def open_door(key):
+        Key(key)
+        keys_store = KeysJsonStore()
+        return keys_store.is_valid(key)
--- src/main/python/secure_all/storage/keys_json_store.py
+++ src/main/python/secure_all/storage/keys_json_store.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .json_store import JsonStore
 from secure_all.cfg.access_manager_config import JSON_FILES_PATH
 from secure_all.exception.access_management_exception import AccessManagementException
@@ -5,24 +7,19 @@ from secure_all.exception.access_management_exception import AccessManagementExc
 
 class KeysJsonStore(JsonStore):
     """Extends json store"""
-    ID_FIELD = "_Access__key"
-    ACCESS_CODE = "_AccessKey_access_code"
-    DNI = "_AccessKey__dni"
-    MAIL_LIST = "_AccessKey_notification_emails"
-    INVALI_ITEM = "Invalid item to be sotored as a key"
-    KEY_ALREADY_STORED = "Key already found in storeRequest"
-
     _FILE_PATH = JSON_FILES_PATH + "storeKeys.json"
-    _ID_FIELD = ID_FIELD
+    _ID_FIELD = "_AccessKey__key"
 
-    # def add_item(self, item):
-    #     """Implementing the restrictions related to avoid duplicated keys"""
-    #     from secure_all.data.access_key import AccessKey
-    #
-    #     if not isinstance(item, AccessKey):
-    #         raise AccessManagementException(self.INVALI_ITEM)
-    #
-    #     if not self.find_item(item.key) is None:
-    #         raise AccessManagementException(self.KEY_ALREADY_STORED)
-    #
-    #     return super().add_item(item)
+    def is_valid(self, key_to_validate):
+        item = self.find_item(key_to_validate)
+        if item is None:
+            raise AccessManagementException("key is not found or is expired")
+
+        justnow = datetime.utcnow()
+        justnow_timestamp = datetime.timestamp(justnow)
+        if item["_AccessKey__expiration_date"] > justnow_timestamp or\
+            item["_AccessKey__expiration_date"] == 0:
+            return True
+
+        raise AccessManagementException("key is not found or is expired")
+
